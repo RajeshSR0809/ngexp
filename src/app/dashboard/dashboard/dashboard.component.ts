5dafd26e1c95abfb21ddee97de3053a57330062e
Rajesh S R <srrajesh.svpur@gmail.com>
2021-04-10 17:30:15 +0530

    on toggle of sidebar, sidebar and content width is adjusted

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, ViewContainerRef } from '@angular/core';
 import { NavService } from 'src/app/core/services/nav.service';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { MatSidenav, MatSidenavContent } from '@angular/material/sidenav';
+import { delay, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,12 +12,37 @@ import { Observable } from 'rxjs';
 export class DashboardComponent implements OnInit {
   opened: boolean = true;
   panelOpenState = false;
-
   navItems$: Observable<any>;
+
+  @ViewChild('sidenav') sidenav: ViewContainerRef;
+  @ViewChild('sideNavContainer') sideNavContainer: ViewContainerRef;
   constructor(private navService: NavService) { }
 
   ngOnInit(): void {
     this.navItems$ = this.navService.getNavItem();
+  }
+
+  toggleSideNav(){
+    this.sidenav['_elementRef'].nativeElement.children[0].classList.toggle('small-sidenav');
+    const n: HTMLElement = this.sideNavContainer['elementRef'].nativeElement;
+    of(1).pipe(take(1),delay(100)).subscribe(_ => {
+      n.style.marginLeft = this.getSideNavMargin();
+    })
+  }
+
+  getSideNavMargin(): string{
+    const sideNavWidth: number = this.sidenav['_elementRef'].nativeElement.clientWidth;
+    //this number is set in the code
+    // ref _side-nav.scss
+
+    // .mat - sidenav.small - sidenav {
+    //   width: 100px;
+    
+    // so it is compared with 100 
+    if(sideNavWidth <= 100){
+      return 101+'px';
+    }
+    return sideNavWidth+'px';
   }
 
 }
